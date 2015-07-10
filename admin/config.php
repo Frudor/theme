97a9c9b1e0b0f34e36e271d9381f88e33053a7ff
Chaitanya Raman <cheky.10@gmail.com>
2015-07-10 06:34:15 +0530

    web

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Frudor/admin/');
+define('HTTP_CATALOG', 'http://localhost/Frudor/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Frudor/admin/');
+define('HTTPS_CATALOG', 'http://localhost/Frudor/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/Frudor/admin/');
+define('DIR_SYSTEM', 'C:/wamp/www/Frudor/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/Frudor/admin/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/Frudor/admin/view/template/');
+define('DIR_CONFIG', 'C:/wamp/www/Frudor/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/Frudor/image/');
+define('DIR_CACHE', 'C:/wamp/www/Frudor/system/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/Frudor/system/download/');
+define('DIR_UPLOAD', 'C:/wamp/www/Frudor/system/upload/');
+define('DIR_LOGS', 'C:/wamp/www/Frudor/system/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/Frudor/system/modification/');
+define('DIR_CATALOG', 'C:/wamp/www/Frudor/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'frudor');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Frudor/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Frudor/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/Frudor/catalog/');
+define('DIR_SYSTEM', 'C:/wamp/www/Frudor/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/Frudor/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/Frudor/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp/www/Frudor/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/Frudor/image/');
+define('DIR_CACHE', 'C:/wamp/www/Frudor/system/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/Frudor/system/download/');
+define('DIR_UPLOAD', 'C:/wamp/www/Frudor/system/upload/');
+define('DIR_MODIFICATION', 'C:/wamp/www/Frudor/system/modification/');
+define('DIR_LOGS', 'C:/wamp/www/Frudor/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'frudor');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
